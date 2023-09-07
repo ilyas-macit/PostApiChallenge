e3e7383e0f8c4895411d7abb3529b00ebc5ff2f0
ilyas-macit <ilyasmacit0@gmail.com>
2023-09-07 20:56:52 +0300

    Mongodb connection settings class and connection configurations created

---
+++ DataAccess/Models/MongoDBSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class MongoDBSettings
+    {
+        public class MongoDbSettings
+        {
+            public string ConnectionURI { get; set; }
+            public string DatabaseName { get; set; }
+            public string CollectionName { get; set; }
+        }
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,7 +1,10 @@
+using DataAccess.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("MongoDb"));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
