7905081407b62f4d4933bd45676be6cfde6d9b55
ilyas-macit <ilyasmacit0@gmail.com>
2023-09-07 21:01:34 +0300

    Data Access layer CRUD operations created

---
+++ DataAccess/Abstracts/IPostDal.cs
@@ -0,0 +1,19 @@
+﻿using Entity.Concrete;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstracts
+{
+    public interface IPostDal
+    {
+        public void Create(Post post);
+        public Post Get(FilterDefinition<Post> filter);
+        public ICollection<Post> GetAll(FilterDefinition<Post> filter = null);
+        public void Update(Post post);
+        public void Delete(string postId);
+    }
+}
---
+++ DataAccess/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using DataAccess.Models;
+using Entity.Concrete;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class ApplicationContext
+    {
+        public IMongoCollection<Post> Posts;
+
+        public ApplicationContext(IOptions<MongoDBSettings> mongoDbSettings)
+        {
+            var client = new MongoClient(mongoDbSettings.Value.ConnectionURI);
+            IMongoDatabase database = client.GetDatabase(mongoDbSettings.Value.DatabaseName);
+            Posts = database.GetCollection<Post>(mongoDbSettings.Value.CollectionName);
+        }
+    }
+}
